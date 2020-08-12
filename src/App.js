6b3988ff30f3b6adc98d6808683d08a9c45ef11b
EdgarLuna8 <55211419+EdgarLuna8@users.noreply.github.com>
2020-08-12 09:51:29 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Logo from './edgar/logo';
+import Footer from './edgarL/footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Logo></Logo>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/edgarL/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './footer.css';
+function footer(){
+    return(
+        <div>
+            <footer>
+                <nav>
+                <a href="https://www.google.con"> Ir a Google  </a>
+                <a href="https://www.facebook.com"> Ir a Facebook </a>
+                <a href="https://www.github.com"> Ir a Github </a>
+                <a href="https://www.youtube.com"> Ir a Youtube </a>
+                <a href="https://www.instagram.com"> Ir a Instagram </a>
+                </nav>
+            </footer>
+        </div>
+    );
+} 
+
+export default footer;
